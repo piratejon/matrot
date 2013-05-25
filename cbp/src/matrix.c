1900f570ba8967c5fe0418e8ee0c5f1156a4891e
Jonathan Wesley Stone <js@piratejon.com>
2013-05-25 18:19:12 -0500

    these tests claim to be passing but i have no proof that they are passing

--- cbp/src/matrix.c
+++ cbp/src/matrix.c
@@ -16,10 +16,20 @@ void free_square_matrix ( SquareMatrix * m )
   free(m);
 }
 
-void fill_matrix_random ( SquareMatrix * m, int row, int col, void * addr ) {
+void fill_matrix_random ( SquareMatrix * m, int row, int col, void * addr )
+{
+  int val = col*m->n + row;
+  memcpy(addr, &val, sizeof(val));
 }
 
 void matrix_fill( SquareMatrix * m, void(*filler)(SquareMatrix *, int, int, void *) )
 {
+  int row, col;
+
+  for ( col = 0; col < m->n; col += 1 ) {
+    for ( row = 0; row < m->n; row += 1 ) {
+      (*filler)(m, row, col, &(m->a[col*m->n + row]));
+    }
+  }
 }
 
